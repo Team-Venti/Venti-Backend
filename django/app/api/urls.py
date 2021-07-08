5498bd115e176a2b2345a7f47e3e0e0639a4b488
jun-k0 <wnsrlsladlek@naver.com>
2021-07-08 21:00:29 +0900

    view.py 기능 협업

--- django/app/api/urls.py
+++ django/app/api/urls.py
@@ -2,6 +2,20 @@ from django.urls import path, include
 from api import views
 from .views import UserViewSet
 from rest_framework.routers import DefaultRouter
+# 기능 구현하고 import ( 밑에 router 연결도 해야함 )
+# 현진
+# from .view_login import ~ViewSet
+# .view_join import ~ViewSet
+# .view_mainpage import ~ViewSet
+# 준기
+# .view_brand import ~ViewSet
+# .view_event import ~ViewSet
+# .view_mypage import ~ViewSet
+# .view_login import ~ViewSet
+# 추가
+# .view_notification import ~ViewSet
+# admin 관련
+
 # CBV
 # urlpatterns = [
 #     path('post/', views.PostList.as_view()),
@@ -11,6 +25,7 @@ from rest_framework.routers import DefaultRouter
 # ViewSet
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
+# router.register()
 urlpatterns = [
     path('', include(router.urls)),
 ]
