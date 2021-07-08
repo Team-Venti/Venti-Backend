ac50e848d30e8bfaa30e9ac5291ad2f519341527
jun-k0 <wnsrlsladlek@naver.com>
2021-07-09 00:45:57 +0900

    모델 수정

--- django/app/api/models.py
+++ django/app/api/models.py
@@ -11,36 +11,37 @@ class DateInfo(models.Model):
 
 
 class User(AbstractUser, DateInfo):
-    birth = models.DateField(null=True)  # superuser 생성을 위해 null=True
+    birth = models.DateField(null=True)
     gender = models.CharField(max_length=10, null=True)  # male / female
 
 
 class Category(DateInfo):
-    category_name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True)
 
 
 class Brand(DateInfo):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="brands")
-    brand_image = models.ImageField()  # imagefield setting.py 추가 설정 필요
-    brand_name = models.CharField(max_length=50)
-    brand_text = models.TextField()
+    image = models.ImageField()  # imagefield setting.py 추가 설정 필요
+    name = models.CharField(max_length=50)
+    text = models.TextField(null=True)
 
 
 class Event(DateInfo):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="events")
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="events")
-    event_name = models.CharField(max_length=50)
-    event_image = models.ImageField()
-    event_banner_image = models.ImageField(null=True)
-    event_text = models.TextField(null=True)
-    event_due = models.DateField()
-    weekly_view = models.DateField()
+    name = models.CharField(max_length=50)
+    image = models.ImageField(null=True)
+    banner_image = models.ImageField(null=True)
+    text = models.TextField(null=True)
+    due = models.DateField(null=True)
+    weekly_view = models.DateField(default=0, null=True)
+    url = models.URLField(null=True)
 
 
 class Notification(DateInfo):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="notifications")
-    notice_type = models.CharField(max_length=10)   # new / finish
+    notice_type = models.CharField(max_length=10)   # new / end
 
 
 class SubscribeBrand(DateInfo):
