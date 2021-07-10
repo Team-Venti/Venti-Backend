8af3276f48dd26f6a237a4466607d5942864922f
jun-k0 <wnsrlsladlek@naver.com>
2021-07-10 21:44:35 +0900

    brand api

--- django/app/api/view_brand.py
+++ django/app/api/view_brand.py
@@ -1,8 +1,22 @@
 from rest_framework import viewsets
 from .models import Brand
 from .serializer_brand import BrandSerializer
+from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+# 브랜드
+
+
+class BrandFilter(FilterSet):
+    category = filters.NumberFilter(field_name="category")
+
+    class Meta:
+        model = Brand
+        fields = ['category']
 
 
 class BrandViewSet(viewsets.ModelViewSet):
     serializer_class = BrandSerializer
     queryset = Brand.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = BrandFilter
