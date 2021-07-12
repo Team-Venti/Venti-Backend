df0b01cddf0c0f0652cf55040378322bada86234
jun-k0 <wnsrlsladlek@naver.com>
2021-07-12 12:21:02 +0900

    (swagger) 이벤트좋아요 수정

--- django/app/api/view_subscribeEvent.py
+++ django/app/api/view_subscribeEvent.py
@@ -8,6 +8,13 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 
 class SubscribeEventFilter(FilterSet):
+    """
+        유저의 이벤트 좋아요 목록을 불러오거나 저장/삭제 하는 API
+        ---
+        # 내용
+            - user : 어떤 유저가 좋아요 했는지 (Foreign Key)
+            - event : 어떤 이벤트를 좋아요 했는지 (Foreign Key)
+    """
     user = filters.NumberFilter(field_name="user")
 
     class Meta:
