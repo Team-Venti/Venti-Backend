99c85b716860ce70e77914ede29c5c1c0ae2d5a7
jun-k0 <wnsrlsladlek@naver.com>
2021-07-13 16:33:26 +0900

    search 기능 추가

--- django/app/api/views_search.py
+++ django/app/api/views_search.py
@@ -10,18 +10,24 @@ from .serializer_event import EventSerializer
 from django_filters.rest_framework import FilterSet, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
+
 # 검색 - 미완
 
 
 class Search(APIView):
     """
-        브랜드 목록을 불러오거나 저장/수정/삭제 하는 API
+        검색한 브랜드/이벤트 목록을 불러오는 API
         ---
         # 내용
-            - 미정
+            - search_event: [검색한 이벤트 목록]
+            - search_brand: [검색한 브랜드 목록]
     """
-    def post(self, request, format=None):
-        data = JSONParser().parse(request)
-        serializer = SearchSerializer(data=data)    # 수정해야함
-        print(serializer)
-        return JsonResponse(serializer.errors,safe=False)
+    def get(self, request, format=None):
+        name = request.GET['name']
+        event = Event.objects.filter(name__contains=name)
+        brand = Brand.objects.filter(name__contains=name)
+
+        return JsonResponse({
+            'search_event': list(event.values()),
+            'search_brand': list(brand.values())
+            }, status=200) # list(result_set)
