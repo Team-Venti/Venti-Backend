ffe574ff787583a4d6bc4a83a85b08fe78e77451
jun-k0 <wnsrlsladlek@naver.com>
2021-07-13 16:32:55 +0900

    brandfilter 추가

---
+++ django/app/api/view_brandfilter.py
@@ -0,0 +1,32 @@
+from django.http import JsonResponse, HttpResponse
+from rest_framework import viewsets
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+
+from .models import Event, Brand
+from .serializer_search import SearchSerializer
+from .serializer_brand import BrandSerializer
+from .serializer_event import EventSerializer
+from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class BrandFilter(APIView):
+    """
+        브랜드 필터링을 거친 이벤트를 불러오는 API
+        ---
+        # 내용
+
+    """
+    # 프엔과 합의해서 category는 어떻게 넘겨줄지 고민
+    def post(self, request, format=None):
+        data = JSONParser().parse(request)
+        arr = []
+        for i in data['brand']:
+            result_set = Event.objects.filter(brand=i)
+            arr.append(list(result_set.values()))
+
+        return JsonResponse({'event_list': arr}, status=200)
+
+    def get(self, request):
+        return HttpResponse(status=200)
