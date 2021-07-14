26fe9e495426725545ba96b425a673798a53177b
jun-k0 <wnsrlsladlek@naver.com>
2021-07-14 11:59:18 +0900

    weekly api 추가

--- django/app/api/view_weekly.py
+++ django/app/api/view_weekly.py
@@ -8,7 +8,6 @@ from .models import Event, Brand
 from .serializer_search import SearchSerializer
 
 
-# 임시니까 다 갈아엎으세용
 class Weekly(APIView):
     """
         인기 이벤트를 불러오거나 저장/수정/삭제 하는 API
@@ -20,4 +19,6 @@ class Weekly(APIView):
     """
 
     def get(self, request, format=None):
-        data = JSONParser().parse(request)
+        hot_event = Event.objects.all().order_by('-weekly_view')
+        result = hot_event.values()
+        return JsonResponse({'result': list(result)}, status=200)
