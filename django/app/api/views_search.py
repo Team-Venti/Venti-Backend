1d16aec3ff849d96601e0e4e77f1c8853d925c0e
gimkuku <khjyj125@gmail.com>
2021-07-24 11:29:00 +0900

    search 수정

--- django/app/api/views_search.py
+++ django/app/api/views_search.py
@@ -1,9 +1,10 @@
+# coding=utf-8
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from .models import Event, Brand
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-
+from rest_framework.response import Response
 response_schema_dict = {
     "200": openapi.Response(
         description="검색 결과를 보여준다.",
@@ -68,12 +69,23 @@ class Search(APIView):
             - search_event: [검색한 이벤트 목록]
             - search_brand: [검색한 브랜드 목록]
     """
-    def get(self, request, format=None):
+    def get(self, request):
         name = request.GET['search']
         event = Event.objects.filter(name__contains=name)
         brand = Brand.objects.filter(name__contains=name)
+        events = []
+        for i in event.values():
+            events.append(i)
 
-        return JsonResponse({
-            'search_event': list(event.values()),
-            'search_brand': list(brand.values())
-            }, status=200) # list(result_set)
+        for i in brand:
+            event_inbrand = Event.objects.filter(brand=i.id)
+            for j in event_inbrand.values():
+                events.append(j)
+
+        # 중복 제거
+        unique = { each['name'] : each for each in events }.values()
+
+        return Response({
+            "events" : unique
+            # 'search_brand': list(brand.values())
+        }, status=200) # list(result_set)
