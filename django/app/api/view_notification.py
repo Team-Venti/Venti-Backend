06cb31d53340c87838fbcdcfefa8a4d25a8bcf78
gimkuku <khjyj125@gmail.com>
2021-07-26 03:02:15 +0900

    마감알람

--- django/app/api/view_notification.py
+++ django/app/api/view_notification.py
@@ -59,18 +59,26 @@ class NotificationUser(APIView):
 
 @permission_classes([])
 @authentication_classes([])
-@background(schedule=1)
-def noti():
+def noti(request):
+    if request.method == "POST":
+        noti_bg(repeat=10)
+        return JsonResponse({"sucess":"true"}, status=302)
+    return JsonResponse({}, status=404)
+
+@background(schedule=10)
+def noti_bg():
     # 현재 시간 가져오기
-    now = datetime.now().strftime("%Y-%m-%d")
+    now = datetime.now().strftime('%Y-%m-%d %h:%m:%s')
     # 12시간 전
-    now12 = datetime.now() - timedelta(hours=12)
+    now12 = datetime.now() + timedelta(hours=12)
     # 12시간 전 이벤트들
-    endevent12 = Event.objects.filter(due=now)
-    Category.objects.create(name=now)
+    endevent12 = Event.objects.filter(due__gte=(now12 - timedelta(minutes=1)),due__lte=(now12 + timedelta(minutes=1)))
+    # Category.objects.create(name=str(endevent12.count())+now)
     # 이 이벤트를 좋아하는 유저 찾기
-    for i in endevent12:
-        likeevents = SubscribeEvent.objects.filter(event = i.id)
-        Category.objects.create(name = i.id)
-        for j in likeevents:
-            Notification.objects.create( user = j.user, event = i.id, brand = i.brand,notice_type = "end", url = "/event/?pk=1")
+    if endevent12.count() != 0:
+        for i in endevent12:
+            likeevents = SubscribeEvent.objects.filter(event = i.id)
+            # Category.objects.create(name = i.name)
+            for j in likeevents:
+                # Category.objects.create(name=j.user)
+                Notification.objects.create(user=j.user, event= i.id, brand=i.brand, notice_type="end")
