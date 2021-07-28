2c99204a0403a6954fe7cad42acedc0b0feb3cca
jun-k0 <wnsrlsladlek@naver.com>
2021-07-28 16:21:25 +0900

    admin에서 이벤트 알림 전송

--- django/app/api/admin.py
+++ django/app/api/admin.py
@@ -5,8 +5,21 @@ from .models import User, Brand, Event, Category, SubscribeBrand, SubscribeEvent
 
 admin.site.register(User)
 admin.site.register(Brand)
-admin.site.register(Event)
+# admin.site.register(Event)
 admin.site.register(Category)
 admin.site.register(SubscribeBrand)
 admin.site.register(SubscribeEvent)
 admin.site.register(Notification)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    actions = ['make_notification']
+
+    def make_notification(self, request, queryset):
+        for i in queryset:
+            subscribe = SubscribeBrand.objects.filter(brand=i.brand.id)
+            for j in subscribe:
+                Notification.objects.create(user=User.objects.get(id=j.user.id))
+
+    make_notification.short_description = '지정 이벤트의 알림 전송'
