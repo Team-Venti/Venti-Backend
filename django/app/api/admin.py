24f7ae9d44ebb46066c1b7e78678550306593db0
jun-k0 <wnsrlsladlek@naver.com>
2021-07-28 18:18:56 +0900

    브랜드에서 배너 브랜드 추가 기능

--- django/app/api/admin.py
+++ django/app/api/admin.py
@@ -1,15 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Brand, Event, Category, SubscribeBrand, SubscribeEvent, Notification
+from .models import User, Brand, Event, Category, SubscribeBrand, SubscribeEvent, Notification, Banner
 
 admin.site.register(User)
-admin.site.register(Brand)
+# admin.site.register(Brand)
 # admin.site.register(Event)
 admin.site.register(Category)
 admin.site.register(SubscribeBrand)
 admin.site.register(SubscribeEvent)
 admin.site.register(Notification)
+admin.site.register(Banner)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    actions = ['make_banner_brand']
+
+    def make_banner_brand(self, request, queryset):
+        for i in queryset:
+            Banner.objects.create(name=i.name, count=0)
+
+    make_banner_brand.short_description = '지정 브랜드의 배너 브랜드 추가'
 
 
 @admin.register(Event)
