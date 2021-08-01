bd82c7a4a2fed63968d19f30abc9185ab7048301
jun-k0 <wnsrlsladlek@naver.com>
2021-08-01 20:32:34 +0900

    admin ui 수정

--- django/app/api/admin.py
+++ django/app/api/admin.py
@@ -3,18 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from .models import User, Brand, Event, Category, SubscribeBrand, SubscribeEvent, Notification, Banner
 
-admin.site.register(User)
+# admin.site.register(User)
 # admin.site.register(Brand)
 # admin.site.register(Event)
-admin.site.register(Category)
-admin.site.register(SubscribeBrand)
-admin.site.register(SubscribeEvent)
-admin.site.register(Notification)
-admin.site.register(Banner)
+# admin.site.register(Category)
+# admin.site.register(SubscribeBrand)
+# admin.site.register(SubscribeEvent)
+# admin.site.register(Notification)
+# admin.site.register(Banner)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'is_superuser', 'is_staff']
+    list_display_links = ['id', 'username']
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category']
+    list_display_links = ['id', 'name']
     actions = ['make_banner_brand']
 
     def make_banner_brand(self, request, queryset):
@@ -26,6 +34,8 @@ class BrandAdmin(admin.ModelAdmin):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'brand', 'due', 'view']
+    list_display_links = ['id', 'name']
     actions = ['make_notification']
 
     def make_notification(self, request, queryset):
@@ -36,3 +46,32 @@ class EventAdmin(admin.ModelAdmin):
 
     make_notification.short_description = '지정 이벤트의 알림 전송'
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+
+
+@admin.register(SubscribeBrand)
+class SubscribeBrandAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'brand']
+    list_display_links = ['id', 'user']
+
+
+@admin.register(SubscribeEvent)
+class SubscribeEventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'event']
+    list_display_links = ['id', 'user']
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'event', 'notice_type', 'brand']
+    list_display_links = ['id', 'user']
+
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'count']
+    list_display_links = ['id', 'name']
